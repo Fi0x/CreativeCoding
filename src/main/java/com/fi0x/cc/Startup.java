a3a668960b83a277b591065158732a9c2dfe1555
Fi0x <felpech@aol.com>
2022-03-14 22:20:53 +0100

    Finished exercise1-1

---
+++ src/main/java/com/fi0x/cc/Startup.java
@@ -0,0 +1,41 @@
+package com.fi0x.cc;
+
+import com.fi0x.cc.week1.Exercise1;
+import processing.core.PApplet;
+
+import java.util.Scanner;
+
+public class Startup
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome to my Creative Coding Lecture Project!");
+        System.out.print("Please type in the number of the exercise you would like to see(1-8): ");
+        String input = sc.next();
+
+        switch(input)
+        {
+            case "1":
+                PApplet.main("com.fi0x.cc.week1.Exercise1");
+                break;
+            case "2":
+                break;
+            case "3":
+                break;
+            case "4":
+                break;
+            case "5":
+                break;
+            case "6":
+                break;
+            case "7":
+                break;
+            case "8":
+                break;
+            default:
+                System.out.println("No valid input, ending the program...");
+                break;
+        }
+    }
+}
---
+++ src/main/java/com/fi0x/cc/week1/Exercise1.java
@@ -0,0 +1,49 @@
+package com.fi0x.cc.week1;
+
+import processing.core.PApplet;
+
+public class Exercise1 extends PApplet
+{
+    private int r = 0;
+    private int g = 0;
+    private int b = 0;
+
+    @Override
+    public void setup()
+    {
+        background(0, 0, 255);
+    }
+    @Override
+    public void settings()
+    {
+        size(500, 400);
+    }
+    @Override
+    public void draw()
+    {
+        stroke(0, 0, 0, 0);
+        fill(r, g, b);
+        rect(250, 200, 150, 100);
+        if(mousePressed)
+        {
+            fill(255, 0, 0);
+            ellipse(mouseX, mouseY, 10, 10);
+        }
+
+        r += Math.random() * 30 - 15;
+        g += Math.random() * 30 - 15;
+        b += Math.random() * 30 - 15;
+        if(r > 255)
+            r = 255;
+        if(g > 255)
+            g = 255;
+        if(b > 255)
+            b = 255;
+        if(r < 0)
+            r = 0;
+        if(g < 0)
+            g = 0;
+        if(b < 0)
+            b = 0;
+    }
+}
