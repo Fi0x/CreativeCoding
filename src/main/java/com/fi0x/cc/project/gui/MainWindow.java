a65a7e23b686a575b0cfe04c2ede89cfd8979334
Fi0x <felpech@aol.com>
2022-03-20 13:55:50 +0100

    Added icon to main window

--- src/main/java/com/fi0x/cc/project/gui/MainWindow.java
+++ src/main/java/com/fi0x/cc/project/gui/MainWindow.java
@@ -1,14 +1,18 @@
 package com.fi0x.cc.project.gui;
 
 import processing.core.PApplet;
+import processing.core.PImage;
 
 public class MainWindow extends PApplet
 {
+    private PImage icon;
+
     @Override
     public void setup()
     {
         surface.setResizable(true);
-//        surface.setIcon();
+        surface.setIcon(icon);
+        surface.setTitle("Computer-Grafik-Projekt");
         surface.setSize(displayWidth / 2, displayHeight / 2);
         surface.setLocation(displayWidth / 4, displayHeight / 4);
         frameRate(60);
@@ -18,5 +22,6 @@ public class MainWindow extends PApplet
     @Override
     public void settings()
     {
+        icon = loadImage("logo.jpg");
     }
 }
