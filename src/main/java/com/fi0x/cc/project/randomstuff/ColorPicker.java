0aeb5ba59257ffcf89f69e34210db4f5bcff468a
Fi0x <felpech@aol.com>
2022-07-23 00:20:00 +0200

    Implemented hue shift with user interaction

--- src/main/java/com/fi0x/cc/project/randomstuff/ColorPicker.java
+++ src/main/java/com/fi0x/cc/project/randomstuff/ColorPicker.java
@@ -10,7 +10,10 @@ public class ColorPicker extends PApplet
     private final PVector SHIFTER_SIZE = new PVector(SELECTOR_SIZE.x, 30);
     private final PVector HEX_DISPLAY_SIZE = new PVector(80, 30);
 
+    private final PVector SELECTOR_TRANSLATION = new PVector();
     private float hueShift = 0;
+    private PVector selectorDragStart;
+    private PVector shifterDragStart;
 
     @Override
     public void settings()
@@ -24,7 +27,7 @@ public class ColorPicker extends PApplet
     @Override
     public void setup()
     {
-        frameRate(60f);
+        frameRate(120f);
         fill(0,0);
         background(255);
         colorMode(HSB, SHIFTER_SIZE.x, SELECTOR_SIZE.x, SELECTOR_SIZE.y, 1);
@@ -41,32 +44,70 @@ public class ColorPicker extends PApplet
 
         translate(SHIFTER_SIZE.x / 2 - HEX_DISPLAY_SIZE.x / 2, SHIFTER_SIZE.y + 10);
         drawHexCode();
+    }
 
-        hueShift++;
-        hueShift %= SHIFTER_SIZE.x;
+    @Override
+    public void mousePressed()
+    {
+        if(mouseButton == RIGHT)
+            return;
 
-        //TODO: Create a HSV color picker with an endless shift-stripe and endless HV-screen
+        if(mouseX >= 10 && mouseX <= SHIFTER_SIZE.x + 10)
+        {
+            if(mouseY >= 10 && mouseY <= SELECTOR_SIZE.y + 10)
+            {
+                selectorDragStart = new PVector(mouseX, mouseY);
+            }
+            else if(mouseY >= 10 + SELECTOR_SIZE.y + 10 && mouseY <= 10 + SELECTOR_SIZE.y + 10 + SHIFTER_SIZE.y)
+            {
+                shifterDragStart = new PVector(mouseX, mouseY);
+            }
+        }
+    }
+    @Override
+    public void mouseReleased()
+    {
+        if(mouseButton == RIGHT)
+            return;
+
+        if(selectorDragStart != null)
+        {
+            PVector translation = calculateSelectorTrans();
+            SELECTOR_TRANSLATION.add(translation);
+            SELECTOR_TRANSLATION.x += SELECTOR_SIZE.x;
+            SELECTOR_TRANSLATION.x %= SELECTOR_SIZE.x;
+            SELECTOR_TRANSLATION.y += SELECTOR_SIZE.y;
+            SELECTOR_TRANSLATION.y %= SELECTOR_SIZE.y;
+            selectorDragStart = null;
+        }
+        else if(shifterDragStart != null)
+        {
+            hueShift = calculateHue();
+            shifterDragStart = null;
+        }
     }
 
     private void drawSelector()
     {
+        //TODO: Translate colors according to transformation AND current mouseDrag
         for(int x = 0; x < SELECTOR_SIZE.x; x++)
         {
             for(int y = 0; y < SELECTOR_SIZE.y; y++)
             {
-                stroke(hueShift, x, SELECTOR_SIZE.y - y);
+                PVector translation = calculateSelectorTrans();
+                stroke(calculateHue(), (x + translation.x) % SELECTOR_SIZE.x, (SELECTOR_SIZE.y - y - translation.y + SELECTOR_SIZE.y) % SELECTOR_SIZE.y);
                 point(x, y);
             }
         }
 
-        stroke((hueShift + SHIFTER_SIZE.x / 2) % SHIFTER_SIZE.x, SELECTOR_SIZE.x, SELECTOR_SIZE.y);
+        stroke((calculateHue() + SHIFTER_SIZE.x / 2) % SHIFTER_SIZE.x, SELECTOR_SIZE.x, SELECTOR_SIZE.y);
         ellipse(SELECTOR_SIZE.x / 2, SELECTOR_SIZE.y / 2, 10, 10);
     }
     private void drawShifter()
     {
         for(int x = 0; x < SHIFTER_SIZE.x; x++)
         {
-            stroke((hueShift + x + SHIFTER_SIZE.x / 2) % SHIFTER_SIZE.x, SELECTOR_SIZE.x, SELECTOR_SIZE.y);
+            stroke((calculateHue() + x + SHIFTER_SIZE.x / 2) % SHIFTER_SIZE.x, SELECTOR_SIZE.x, SELECTOR_SIZE.y);
             line(x, 0, x, SHIFTER_SIZE.y);
         }
 
@@ -84,5 +125,28 @@ public class ColorPicker extends PApplet
         textAlign(PConstants.CENTER, PConstants.CENTER);
         text(hex(selectedColor), HEX_DISPLAY_SIZE.x / 2, HEX_DISPLAY_SIZE.y / 2);
         fill(0, 0);
+    }
+
+    private float calculateHue()
+    {
+        float shiftedDistance = shifterDragStart == null ? 0 : shifterDragStart.x - mouseX;
+        float hueShifted = hueShift + shiftedDistance;
+        while(hueShifted < 0)
+            hueShifted += SHIFTER_SIZE.x;
+        return hueShifted % SHIFTER_SIZE.x;
+    }
+    private PVector calculateSelectorTrans()
+    {
+        float translatedXDistance = selectorDragStart == null ? 0 : selectorDragStart.x - mouseX;
+        float xTranslated = SELECTOR_TRANSLATION.x + translatedXDistance;
+        while(xTranslated < 0)
+            xTranslated += SELECTOR_SIZE.x;
+
+        float translatedYDistance = selectorDragStart == null ? 0 : selectorDragStart.y - mouseY;
+        float yTranslated = SELECTOR_TRANSLATION.y + translatedYDistance;
+        while(yTranslated < 0)
+            yTranslated += SELECTOR_SIZE.y;
+
+        return new PVector(xTranslated % SELECTOR_SIZE.x, yTranslated % SELECTOR_SIZE.y);
     }
 }
